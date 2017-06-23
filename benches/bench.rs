5cf68c77e988e6a103ecabc7198f8faa26d1be6a
David Roundy <roundyd@physics.oregonstate.edu>
2017-06-23 09:02:09 -0700

    first draft of a SmallSet

---
+++ benches/bench.rs
@@ -0,0 +1,98 @@
+#![feature(test)]
+
+extern crate test;
+extern crate smallset;
+
+use test::Bencher;
+use std::collections::HashSet;
+use smallset::SmallSet;
+
+macro_rules! missing_bench {
+    ($i: expr, $name_small: ident, $name_hash: ident) => {
+        #[bench]
+        fn $name_small(b: &mut Bencher) {
+            let set: SmallSet<usize> = (0..$i).collect();
+            let not_here = $i+1;
+            b.iter(|| set.contains(&not_here));
+        }
+        #[bench]
+        fn $name_hash(b: &mut Bencher) {
+            let set: HashSet<usize> = (0..$i).collect();
+            let not_here = $i+1;
+            b.iter(|| set.contains(&not_here));
+        }
+    };
+}
+
+missing_bench!(0, missing_smallset_00, missing_hashset_00);
+missing_bench!(1, missing_smallset_01, missing_hashset_01);
+missing_bench!(2, missing_smallset_02, missing_hashset_02);
+missing_bench!(3, missing_smallset_03, missing_hashset_03);
+missing_bench!(4, missing_smallset_04, missing_hashset_04);
+missing_bench!(5, missing_smallset_05, missing_hashset_05);
+missing_bench!(6, missing_smallset_06, missing_hashset_06);
+missing_bench!(7, missing_smallset_07, missing_hashset_07);
+missing_bench!(8, missing_smallset_08, missing_hashset_08);
+missing_bench!(9, missing_smallset_09, missing_hashset_09);
+missing_bench!(10, missing_smallset_10, missing_hashset_10);
+missing_bench!(99, missing_smallset_99, missing_hashset_99);
+
+macro_rules! present_bench {
+    ($i: expr, $name_small: ident, $name_hash: ident) => {
+        #[bench]
+        fn $name_small(b: &mut Bencher) {
+            let here = $i-1;
+            let set: SmallSet<usize> = (0..$i).collect();
+            b.iter(|| set.contains(&here));
+        }
+        #[bench]
+        fn $name_hash(b: &mut Bencher) {
+            let here = $i-1;
+            let set: HashSet<usize> = (0..$i).collect();
+            b.iter(|| set.contains(&here));
+        }
+    };
+}
+
+present_bench!(0, present_smallset_00, present_hashset_00);
+present_bench!(1, present_smallset_01, present_hashset_01);
+present_bench!(2, present_smallset_02, present_hashset_02);
+present_bench!(3, present_smallset_03, present_hashset_03);
+present_bench!(4, present_smallset_04, present_hashset_04);
+present_bench!(5, present_smallset_05, present_hashset_05);
+present_bench!(6, present_smallset_06, present_hashset_06);
+present_bench!(7, present_smallset_07, present_hashset_07);
+present_bench!(8, present_smallset_08, present_hashset_08);
+present_bench!(9, present_smallset_09, present_hashset_09);
+present_bench!(10, present_smallset_10, present_hashset_10);
+present_bench!(99, present_smallset_99, present_hashset_99);
+
+macro_rules! collect_bench {
+    ($i: expr, $name_small: ident, $name_hash: ident) => {
+        #[bench]
+        fn $name_small(b: &mut Bencher) {
+            b.iter(|| {
+                let s: SmallSet<usize> = (0..$i).collect();
+                s
+            });
+        }
+        #[bench]
+        fn $name_hash(b: &mut Bencher) {
+            b.iter(|| {
+                let s: HashSet<usize> = (0..$i).collect();
+                s
+            });
+        }
+    };
+}
+
+collect_bench!( 1, collect_smallset_01, collect_hashset_01);
+collect_bench!( 2, collect_smallset_02, collect_hashset_02);
+collect_bench!( 3, collect_smallset_03, collect_hashset_03);
+collect_bench!( 4, collect_smallset_04, collect_hashset_04);
+collect_bench!( 5, collect_smallset_05, collect_hashset_05);
+collect_bench!( 6, collect_smallset_06, collect_hashset_06);
+collect_bench!( 7, collect_smallset_07, collect_hashset_07);
+collect_bench!( 8, collect_smallset_08, collect_hashset_08);
+collect_bench!( 9, collect_smallset_09, collect_hashset_09);
+collect_bench!(99, collect_smallset_99, collect_hashset_99);
---
+++ src/lib.rs
@@ -0,0 +1,173 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
+pub const CAPACITY: usize = 8;
+
+#[derive(Debug, Clone)]
+pub struct SmallSet<T: Copy + Eq + Hash> {
+    inner: SS<T>,
+}
+
+#[derive(Debug, Clone)]
+enum SS<T: Copy+Eq+Hash> {
+    Small(usize, [T;CAPACITY]),
+    Large(HashSet<T>),
+}
+
+pub struct Iter<'a, T: 'a+Copy+Eq+Hash> {
+    inner: It<'a, T>,
+}
+
+enum It<'a, T: 'a+Copy+Eq+Hash> {
+    Small(std::slice::Iter<'a, T>),
+    Large(std::collections::hash_set::Iter<'a, T>),
+}
+
+impl<T: Copy+Eq+Hash> SmallSet<T> {
+    pub fn new() -> SmallSet<T> {
+        SmallSet { inner: SS::Small(0, unsafe { std::mem::uninitialized() }) }
+    }
+    pub fn with_capacity(cap: usize) -> SmallSet<T> {
+        if cap > CAPACITY {
+            SmallSet { inner: SS::Large(HashSet::with_capacity(cap)) }
+        } else {
+            SmallSet::new()
+        }
+    }
+    pub fn len(&self) -> usize {
+        match self.inner {
+            SS::Large(ref s) => s.len(),
+            SS::Small(len, _) => len,
+        }
+    }
+    pub fn reserve(&mut self, additional: usize) {
+        match self.inner {
+            SS::Large(ref mut s) => {
+                s.reserve(additional);
+                return;
+            },
+            SS::Small(len, arr) => {
+                let mut s = HashSet::with_capacity(additional+CAPACITY);
+                for i in 0..len {
+                    s.insert(arr[i]);
+                }
+                *self = SmallSet { inner: SS::Large(s) }
+            },
+        }
+    }
+    pub fn insert(&mut self, elem: T) -> bool {
+        match self.inner {
+            SS::Large(ref mut s) => {
+                return s.insert(elem);
+            },
+            SS::Small(ref mut len, ref mut arr) => {
+                for i in 0 .. *len {
+                    if arr[i] == elem {
+                        return false;
+                    }
+                }
+                if *len < CAPACITY-1 {
+                    arr[*len] = elem;
+                    *len += 1;
+                    return true;
+                }
+            },
+        }
+        match self.inner {
+            SS::Large(_) => unreachable!(),
+            SS::Small(len, arr) => {
+                let mut s = HashSet::with_capacity(1+CAPACITY);
+                for i in 0..len {
+                    s.insert(arr[i]);
+                }
+                s.insert(elem);
+                *self = SmallSet { inner: SS::Large(s) };
+                true
+            },
+        }
+    }
+    pub fn contains(&self, elem: &T) -> bool {
+        match self.inner {
+            SS::Large(ref s) => {
+                s.contains(elem)
+            },
+            SS::Small(len, ref arr) => {
+                for i in 0 .. len {
+                    if arr[i] == *elem {
+                        return true;
+                    }
+                }
+                false
+            },
+        }
+    }
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            inner:
+            match self.inner {
+                SS::Large(ref s) => {
+                    It::Large(s.iter())
+                },
+                SS::Small(len, ref arr) => {
+                    It::Small(arr[0..len].iter())
+                },
+            }
+        }
+    }
+}
+
+impl<T: Hash+Copy+Eq> std::iter::FromIterator<T> for SmallSet<T> {
+    fn from_iter<I: IntoIterator<Item=T>>(iter: I) -> Self {
+        let iter = iter.into_iter();
+        let (sz,_) = iter.size_hint();
+        let mut c = SmallSet::with_capacity(sz);
+        for i in iter {
+            c.insert(i);
+        }
+        c
+    }
+}
+
+
+impl<'a, T: 'a+Eq+Hash+Copy> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<&'a T> {
+        match self.inner {
+            It::Large(ref mut it) => it.next(),
+            It::Small(ref mut it) => it.next(),
+        }
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match self.inner {
+            It::Large(ref it) => it.size_hint(),
+            It::Small(ref it) => it.size_hint(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let mut ss: SmallSet<usize> = SmallSet::new();
+        ss.insert(5);
+        assert!(ss.contains(&5));
+        assert!(!ss.contains(&4));
+        ss.insert(3);
+        println!("now {:?}", &ss);
+        assert!(ss.contains(&3));
+        assert!(ss.contains(&5));
+        assert!(ss.len() == 2);
+        for num in ss.iter() {
+            assert!(ss.contains(num));
+        }
+    }
+    #[test]
+    fn size_unwasted() {
+        println!("small size: {}", std::mem::size_of::<SmallSet<usize>>());
+        println!(" hash size: {}", std::mem::size_of::<HashSet<usize>>());
+        assert!(std::mem::size_of::<SmallSet<usize>>() <=
+                2*std::mem::size_of::<HashSet<usize>>());
+    }
+}
