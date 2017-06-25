726288fbfd83f8fe7f98059394b4c9bd88c5027b
David Roundy <roundyd@physics.oregonstate.edu>
2017-06-25 07:19:46 -0700

    enable benchmarking of allocations (nightly)

--- bench/src/main.rs
+++ bench/src/main.rs
@@ -1,3 +1,4 @@
+extern crate david_allocator;
 extern crate david_set;
 extern crate rand;
 
@@ -11,25 +12,23 @@ use david_set::Set;
 
 macro_rules! initialize {
     ($set: ident, $item: ident, $num: expr) => {{
-        let mut set = $set::<$item>::new();
         let mut rng = XorShiftRng::from_seed([1,2,3,4]);
+        let before = david_allocator::net_allocation();
+        let before_total = david_allocator::total_allocation();
+        let mut set = $set::<$item>::new();
         if $num > 0 {
             while set.len() < $num {
                 set.insert($item::rand(&mut rng) % (2*$num as $item));
                 set.remove(& ($item::rand(&mut rng) % (2*$num as $item)));
             }
         }
+        let net_alloced = david_allocator::net_allocation() - before;
+        let total_alloced = david_allocator::total_allocation() - before_total;
         let mut unused = $item::rand(&mut rng);
         while set.contains(&unused) {
             unused = $item::rand(&mut rng);
         }
-        let all_present: Vec<_> = set.iter().map(|&x| x).collect();
-        let present = if $num > 0 {
-            all_present[usize::rand(&mut rng) % all_present.len()]
-        } else {
-            $item::rand(&mut rng)
-        };
-        (set, unused, present)
+        (set, unused, std::mem::size_of::<$set<$item>>(), net_alloced, total_alloced)
     }};
 }
 
@@ -53,48 +52,61 @@ fn sorted<T: IntoIterator>(iter: T) -> Vec<T::Item>
 
 fn main() {
     let iters = 10000000;
-    println!("{:10}{:>6}{:>9}{:>9}{:>11}{:>11}{:>11}",
-             "contains", "size", "set/hash", "btree/hash",
-             "set (s)", "btree (s)", "hash (s)");
+    println!("{:10} {:>5} {:>8} ({:>4}/{:>4}/{:>4}) {:>8} ({:4}/{:4}/{:4})",
+             "contains", "size",
+             "set/hash", "stac", "heap","allo",
+             "btree/hash", "stac", "heap","allo");
     for size in (1..15).chain([20,30,50,100,1000,10000].iter().map(|&x|x)) {
-        let (set, mut unused, _) = initialize!(Set, usize, size);
+        let (set, mut unused, set_stack, set_size, set_total)
+            = initialize!(Set, usize, size);
         //println!("size {} {:?}", set.len(), sorted(&set));
         let mut total = 0;
         let set_time = time_me!({
             unused = (unused+1)%(2*size);
             if set.contains(&unused) { total += 1; }
         }, iters);
-        let set_total = total;
+        let set_total_true = total;
 
-        let (set, mut unused, _) = initialize!(HashSet, usize, size);
+        let (set, mut unused, hash_stack, hash_size, hash_total)
+            = initialize!(HashSet, usize, size);
         let mut total = 0;
         let hash_time = time_me!({
             unused = (unused+1)%(2*size);
             if set.contains(&unused) { total += 1; }
         }, iters);
-        if total != set_total {
+        if total != set_total_true {
             println!("serious problem with hash!");
         }
 
-        let (set, mut unused, _) = initialize!(BTreeSet, usize, size);
+        let (set, mut unused, btree_stack, btree_size, btree_total)
+            = initialize!(BTreeSet, usize, size);
         let mut total = 0;
         let btree_time = time_me!({
             unused = (unused+1)%(2*size);
             if set.contains(&unused) { total += 1; }
         }, iters);
-        if total != set_total {
+        if total != set_total_true {
             println!("serious problem with btree!");
         }
 
-        println!("{:10} {:5} {:8.5} {:8.5} {:10.6} {:10.6} {:10.6}",
-                 "", size, set_time/hash_time, btree_time/hash_time,
-                 set_time, btree_time, hash_time);
+        println!("{:10} {:5} {:8.5} ({:4.2}/{:4.2}/{:4.2}) {:8.5} ({:4.2}/{:4.2}/{:4.2})",
+                 "", size,
+                 set_time/hash_time,
+                 ((set_stack+set_size) as f64/(hash_stack + hash_size) as f64),
+                 (set_size as f64/hash_size as f64),
+                 (set_total as f64/hash_total as f64),
+                 btree_time/hash_time,
+                 ((btree_stack+btree_size) as f64/(hash_stack + hash_size) as f64),
+                 (btree_size as f64/hash_size as f64),
+                 (btree_total as f64/hash_total as f64));
     }
-    println!("{:10}{:>6}{:>9}{:>9}{:>11}{:>11}{:>11}",
-             "remove/insert", "size", "set/hash", "btree/hash",
-             "set (s)", "btree (s)", "hash (s)");
+    println!("{:10} {:>5} {:>8} ({:>4}/{:>4}/{:>4}) {:>8} ({:4}/{:4}/{:4})",
+             "remove/ins", "size",
+             "set/hash", "stac", "heap","allo",
+             "btree/hash", "stac", "heap","allo");
     for size in (1..15).chain([20,30,50,100,1000,10000].iter().map(|&x|x)) {
-        let (mut set, _, _) = initialize!(Set, usize, size);
+        let (mut set, _, set_stack, set_size, set_total)
+            = initialize!(Set, usize, size);
         let mut next = 0;
         let set_time = time_me!({
             if set.remove(&next) {
@@ -104,7 +116,8 @@ fn main() {
             next %= 2*size;
         }, iters);
 
-        let (mut set, _, _) = initialize!(HashSet, usize, size);
+        let (mut set, _, hash_stack, hash_size, hash_total)
+            = initialize!(HashSet, usize, size);
         let hash_time = time_me!({
             if set.remove(&next) {
                 set.insert(next);
@@ -113,7 +126,8 @@ fn main() {
             next %= 2*size;
         }, iters);
 
-        let (mut set, _, _) = initialize!(BTreeSet, usize, size);
+        let (mut set, _, btree_stack, btree_size, btree_total)
+            = initialize!(BTreeSet, usize, size);
         let btree_time = time_me!({
             if set.remove(&next) {
                 set.insert(next);
@@ -122,9 +136,16 @@ fn main() {
             next %= 2*size;
         }, iters);
 
-        println!("{:10} {:5} {:8.5} {:8.5} {:10.6} {:10.6} {:10.6}",
-                 "", size, set_time/hash_time, btree_time/hash_time,
-                 set_time, btree_time, hash_time);
+        println!("{:10} {:5} {:8.5} ({:4.2}/{:4.2}/{:4.2}) {:8.5} ({:4.2}/{:4.2}/{:4.2})",
+                 "", size,
+                 set_time/hash_time,
+                 ((set_stack+set_size) as f64/(hash_stack + hash_size) as f64),
+                 (set_size as f64/hash_size as f64),
+                 (set_total as f64/hash_total as f64),
+                 btree_time/hash_time,
+                 ((btree_stack+btree_size) as f64/(hash_stack + hash_size) as f64),
+                 (btree_size as f64/hash_size as f64),
+                 (btree_total as f64/hash_total as f64));
     }
 }
 
---
+++ david_allocator/src/lib.rs
@@ -0,0 +1,83 @@
+// The compiler needs to be instructed that this crate is an allocator in order
+// to realize that when this is linked in another allocator like jemalloc should
+// not be linked in
+#![feature(allocator)]
+#![allocator]
+
+// Allocators are not allowed to depend on the standard library which in turn
+// requires an allocator in order to avoid circular dependencies. This crate,
+// however, can use all of libcore.
+#![no_std]
+
+// Let's give a unique name to our custom allocator
+#![crate_name = "david_allocator"]
+#![crate_type = "rlib"]
+
+// Our system allocator will use hand-written FFI bindings. Note that
+// currently the external (crates.io) libc cannot be used because it
+// links to the standard library (e.g. `#![no_std]` isn't stable yet),
+// so that's why this specifically requires the in-tree version.
+extern "C" {
+    fn malloc(sz: usize) -> *mut u8;
+    fn free(ptr: *mut u8);
+    fn realloc(ptr: *mut u8, sz: usize) -> *mut u8;
+}
+
+// Listed below are the five allocation functions currently required by custom
+// allocators. Their signatures and symbol names are not currently typechecked
+// by the compiler, but this is a future extension and are required to match
+// what is found below.
+//
+// Note that the standard `malloc` and `realloc` functions do not provide a way
+// to communicate alignment so this implementation would need to be improved
+// with respect to alignment in that aspect.
+
+static mut NET: usize = 0;
+static mut TOTAL: usize = 0;
+
+#[no_mangle]
+pub extern fn __rust_allocate(size: usize, _align: usize) -> *mut u8 {
+    unsafe {
+        NET += size;
+        TOTAL += size;
+        malloc(size)
+    }
+}
+
+#[no_mangle]
+pub extern fn __rust_deallocate(ptr: *mut u8, old_size: usize, _align: usize) {
+    unsafe {
+        NET -= old_size;
+        free(ptr)
+    }
+}
+
+#[no_mangle]
+pub extern fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize,
+                                _align: usize) -> *mut u8 {
+    unsafe {
+        TOTAL += size;
+        NET += size;
+        NET -= old_size;
+        realloc(ptr, size)
+    }
+}
+
+#[no_mangle]
+pub extern fn __rust_reallocate_inplace(_ptr: *mut u8, old_size: usize,
+                                        _size: usize, _align: usize) -> usize {
+    old_size // this api is not supported by libc
+}
+
+#[no_mangle]
+pub extern fn __rust_usable_size(size: usize, _align: usize) -> usize {
+    size
+}
+
+pub fn net_allocation() -> usize {
+    unsafe { NET }
+}
+
+pub fn total_allocation() -> usize {
+    unsafe { TOTAL }
+}
