8959b0a5b81ca77d8e95cb13a1dc317ce2db9800
David Roundy <roundyd@physics.oregonstate.edu>
2017-06-29 08:38:11 -0700

    do not want to cargo run in CI tests

--- ci/script.sh
+++ ci/script.sh
@@ -13,8 +13,8 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    # cross run --target $TARGET
+    # cross run --target $TARGET --release
 }
 
 if [ -z $TRAVIS_TAG ]; then
