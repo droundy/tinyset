feee62ba756bb5f3faaec67b19129da72ab3cfe4
David Roundy <roundyd@physics.oregonstate.edu>
2018-02-26 08:46:47 -0800

    bump version and relicense as apache/mit

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright 2017-2018 David Roundy
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 //! `tinyset` contains a few collections that are optimized to scale
 //! in size well for small numbers of elements, while still scaling
 //! well in time (and size) for numbers of elements.  We have two set types:
