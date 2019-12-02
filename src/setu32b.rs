9aaa82c419fcf7c2bdcd8307ed89a479879a1915
David Roundy <roundyd@physics.oregonstate.edu>
2019-12-01 16:20:43 -0800

    add inserstion into Tiny

--- src/setu32b.rs
+++ src/setu32b.rs
@@ -28,8 +28,12 @@ impl Iterator for Tiny {
     fn size_hint(&self) -> (usize, Option<usize>) {
         (self.bits.count_ones()  as usize, Some(self.bits.count_ones() as usize))
     }
-    fn min(mut self) -> Option<u32> {
-        self.next()
+    fn min(self) -> Option<u32> {
+        Some(self.start + self.bits.trailing_zeros())
+    }
+    fn max(self) -> Option<u32> {
+        println!("leading zeros {}", self.bits.leading_zeros());
+        Some(self.start + 64 - self.bits.leading_zeros())
     }
 }
 
@@ -38,6 +42,7 @@ impl Tiny {
     fn to_usize(self) -> usize {
         (self.start as usize) << 32 | self.bits << 2 | 1
     }
+    #[cfg(target_pointer_width = "64")]
     fn from_usize(x: usize) -> Self {
         Tiny {
             start: (x >> 32) as u32,
@@ -50,8 +55,42 @@ impl Tiny {
     fn len(&self) -> usize {
         self.bits.count_ones() as usize
     }
+    fn contains(&self, v: u32) -> bool {
+        v >= self.start && v <= self.start + 30 && self.bits >> (v-self.start) & 1 != 0
+    }
+    fn insert(&mut self, v: u32) -> Option<bool> {
+        if self.contains(v) {
+            return Some(true);
+        }
+        let mx = self.clone().max().unwrap();
+        let mn = self.clone().min().unwrap();
+        if v + 30 < mx || v > mn + 30 {
+            None
+        } else if v < mn {
+            self.bits = self.bits << (self.start-v) | 1;
+            self.start = v;
+            Some(false)
+        } else if self.start < mn {
+            self.bits = self.bits >> (mn - self.start) | 1 << (v-mn);
+            self.start = mn;
+            Some(false)
+        } else {
+            self.bits = self.bits | 1 << (v-self.start);
+            Some(false)
+        }
+    }
 }
 
+#[test]
+fn check_tiny_insert() {
+    let mut t = Tiny::from_singleton(0).unwrap();
+    println!("starting with {:?}", t.clone().collect::<Vec<_>>());
+    for v in [0,1,1,2,29,30].into_iter().cloned() {
+        assert_eq!(Some(t.contains(v)), t.insert(v));
+        println!(" after inserting {}: {:?}", v, t.clone().collect::<Vec<_>>());
+        assert!(t.contains(v));
+    }
+}
 #[cfg(test)]
 proptest!{
     #[test]
@@ -59,7 +98,18 @@ proptest!{
         let t = Tiny::from_singleton(x).unwrap();
         assert_eq!(t.clone().next(), Some(x));
         assert_eq!(t.clone().count(), 1);
+        assert!(t.contains(x));
+        assert!(!t.contains(x+1));
         assert_eq!(t.len(), 1);
+    }
+    #[test]
+    fn check_tiny_from_inserts(x0: u32,
+                               vals in prop::collection::vec(0..28u32, 1usize..10)) {
+        let mut t = Tiny::from_singleton(x0).unwrap();
+        for v in vals.iter().cloned().map(|v| v+x0) {
+            assert_eq!(Some(t.contains(v)), t.insert(v));
+            assert!(t.contains(v));
+        }
     }
     #[test]
     fn check_to_from_tiny(x: usize) {
