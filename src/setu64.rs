a0cb1c611a7da4c32c2d9046711e523de326c3c6
Leonid Ryzhyk <ryzhyk@gmail.com>
2020-05-08 17:55:26 -0700

    Add Send and Sync impls for SetU64 and SetU32. (#9)
    Added Send and Sync impls for SetU64 and SetU32 as per discussion with
    @droundy:
    
    See https://github.com/droundy/tinyset/issues/8

--- src/setu64.rs
+++ src/setu64.rs
@@ -53,6 +53,9 @@ fn unsplit_u64(k: u64, offset: u64, bits: u64) -> u64 {
 /// A set of u64
 pub struct SetU64(*mut S);
 
+unsafe impl Send for SetU64 {}
+unsafe impl Sync for SetU64 {}
+
 impl std::fmt::Debug for SetU64 {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         write!(f, "SetU64 {:?}", self.iter().collect::<Vec<_>>())?;
