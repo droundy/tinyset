719a040603193b914f6b273603326bb6adbccc40
David Roundy <roundyd@physics.oregonstate.edu>
2020-06-19 10:44:21 -0700

    remove commented C code

--- src/rand.rs
+++ src/rand.rs
@@ -28,10 +28,6 @@ pub fn rand64() -> u64 {
     let z = (z ^ (z >> 30)) * Wrapping(0xbf58476d1ce4e5b9);
 	let z = (z ^ (z >> 27)) * Wrapping(0x94d049bb133111eb);
 	(z ^ (z >> 31)).0
-    // uint64_t z = (x += 0x9e3779b97f4a7c15);
-	// z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
-	// z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
-	// return z ^ (z >> 31);
 }
 
 #[cfg(feature = "rand")]
